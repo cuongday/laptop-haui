45d421dd92bb07d1b9d88cf3a576fafe581175d3
huyMK-147 <anhuypc147@gmail.com>
2024-06-03 15:05:13 +0700

    policy

--- src/main/java/com/ndc/laptopvn/controller/client/WarrantyController.java
+++ src/main/java/com/ndc/laptopvn/controller/client/WarrantyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WarrantyController {
     @GetMapping("/warranty")
     public String getWarrantyPage(){
-        return "client/Warranty/warranty";
+        return "client/policy/warranty";
     }
 
 }
